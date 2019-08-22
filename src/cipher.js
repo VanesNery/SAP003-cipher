9d23c0950168711314dc2e52563c564591d8bfc9
Vanessa Nery <54214968+VanesNery@users.noreply.github.com>
2019-08-22 17:31:23 -0300

    Update cipher.js
    inserindo o offset negativo

--- src/cipher.js
+++ src/cipher.js
@@ -38,36 +38,6 @@ function encode(offset,string){
  }
    
    function decode(offset,string){
-      let Mssg=[];
-         for(let i=0; i<string.length;i++){
-         let letra=string.charCodeAt([i]);
-            
-            if(letra>=65 && letra<=90){
-            let message=((letra -90 - offset)%26) +90;
-            let msg=String.fromCharCode(message); 
-            Mssg.push(msg);
-            }
-               else if(letra>=97 && letra<=122){
-               let message=((letra -122 - offset)%26) +122;
-               let msg=String.fromCharCode(message); 
-               Mssg.push(msg);
-               }
-                  else if(letra>=128 && letra<=237){
-                  let message=((letra -237 - offset)%110) +237;
-                  let msg=String.fromCharCode(message); 
-                  Mssg.push(msg);
-                  }
-                     else if(letra>=32 && letra<=47){
-                     let message=((letra -47 - offset)%33) +47;
-                     let msg=String.fromCharCode(message); 
-                     Mssg.push(msg);
-                     }
-                        else if(letra>=238 && letra<=255){
-                        let message=((letra -255 - offset)%18) +255;
-                        let msg=String.fromCharCode(message); 
-                        Mssg.push(msg);
-                        }
-         }
-            return Mssg.join("");
+      return encode (string,-offset);
    }
    //Decifra (((codigoasc - cod1letra- deslo) %tamanhoDOalfabeto) + cod1letra)
