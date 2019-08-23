edff6bc664f8dc86a090ed95925e5f43e4b861da
VanesNery <nery_van@hotmail.com>
2019-08-23 02:33:58 -0300

    Ajustando cipher e atualizando css

--- src/cipher.js
+++ src/cipher.js
@@ -18,55 +18,45 @@ function encode(offset,string){
                let msg=String.fromCharCode(message); 
                Mssg.push(msg);
                }
-                  else if(letra>=128 && letra<=237){
-                  let message=((letra -128 + offset)%110) +128;
+                  else if(letra>=48 && letra<=57){
+                  let message=((letra -48 + offset)%10) +48;
                   let msg=String.fromCharCode(message); 
                   Mssg.push(msg);
                   }
-                     else if(letra>=32 && letra<=64){
-                     let message=((letra -32 + offset)%33) +32;
+                     else if(letra>=0 && letra<=47 || letra>=56 && letra<=64|| letra>=91 && letra<=96 || letra>=123 && letra<=255){
+                     let message=letra;
                      let msg=String.fromCharCode(message); 
                      Mssg.push(msg);
                      }
-                        else if(letra>=238 && letra<=255){
-                        let message=((letra -238 + offset)%18) +238;
-                        let msg=String.fromCharCode(message); 
-                        Mssg.push(msg);
-                        }
             }
                   return Mssg.join("");
  }
-   
-   function decode(offset,string){
-      let Mssg=[];
-         for(let i=0; i<string.length;i++){
-         let letra=string.charCodeAt([i]);
+        
+       function decode(offset,string){
+       let Mssg=[];
+          for(let i=0; i<string.length;i++){
+          let letra=string.charCodeAt([i]);
             
-            if(letra>=65 && letra<=90){
-            let message=((letra -90 - offset)%26) +90;
-            let msg=String.fromCharCode(message); 
-            Mssg.push(msg);
-            }
-               else if(letra>=97 && letra<=122){
-               let message=((letra -122 - offset)%26) +122;
-               let msg=String.fromCharCode(message); 
-               Mssg.push(msg);
-               }
-                  else if(letra>=128 && letra<=237){
-                  let message=((letra -237 - offset)%110) +237;
-                  let msg=String.fromCharCode(message); 
-                  Mssg.push(msg);
-                  }
-                     else if(letra>=32 && letra<=47){
-                     let message=((letra -47 - offset)%33) +47;
-                     let msg=String.fromCharCode(message); 
-                     Mssg.push(msg);
-                     }
-                        else if(letra>=238 && letra<=255){
-                        let message=((letra -255 - offset)%18) +255;
-                        let msg=String.fromCharCode(message); 
-                        Mssg.push(msg);
-                        }
+             if(letra>=65 && letra<=90){
+             let message=((letra -90 - offset)%26) +90;
+             let msg=String.fromCharCode(message); 
+             Mssg.push(msg);
+             }
+                else if(letra>=97 && letra<=122){
+                let message=((letra -122 - offset)%26) +122;
+                let msg=String.fromCharCode(message); 
+                Mssg.push(msg);
+                }
+                   else if(letra>=48 && letra<=57){
+                   let message=((letra -57 - offset)%10) +57;
+                   let msg=String.fromCharCode(message); 
+                   Mssg.push(msg);
+                   }
+                      else if(letra>=0 && letra<=64 || letra>=91 && letra<=96 || letra>=123 && letra<=255){
+                      let message=letra;
+                      let msg=String.fromCharCode(message); 
+                      Mssg.push(msg);
+                      }
          }
             return Mssg.join("");
    }
